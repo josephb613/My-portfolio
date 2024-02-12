7bc5904b5a877179370fa05388b787372178a99b
Joseph basix <josephbasix@gmail.com>
2024-02-13 00:01:43 +0100

    init commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,11 +1,11 @@
-const logotext = "JOHN";
+const logotext = "Joseph";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Joseph basix",
+    description: "I’m Joseph basix Full stack developer",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Joseph basix",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
